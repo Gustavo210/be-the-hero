9e23e2b5f7317d117256378eb6bc2f6121d2879c
Gustavo Aurelio <gustavogontijoo@gmail.com>
2020-04-10 10:43:16 -0300

    aplicação finalizada e textada

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors} = require('celebrate');
 const routes = require('./routes');
 const app = express();
 
@@ -36,5 +37,6 @@ app.use(express.json());
   */
 
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
